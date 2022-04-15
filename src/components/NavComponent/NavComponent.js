0e25519a131df9ccce54ba14b655261c741887f6
Carlos Fernandez <fdez.carlos303@gmail.com>
2022-04-14 22:53:54 -0400

    tweaks

--- src/components/NavComponent/NavComponent.js
+++ src/components/NavComponent/NavComponent.js
@@ -1,7 +1,6 @@
 import "./NavComponent.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavComponent = () => {
